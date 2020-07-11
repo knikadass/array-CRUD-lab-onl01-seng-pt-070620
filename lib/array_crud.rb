123cd4d3bee923518059b70407bee79cd0962e47
alluring-blog-5170 <knikadass@gmail.com>
2020-07-11 01:23:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   famous_dogs = ["Simba", "Kobe", "Bentley", "Nala"]
-  famous_dogs.pop("Nala")
+  famous_dogs.pop
 end
 
 def remove_element_from_start_of_array(array)
