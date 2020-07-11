e29c73c92aaca059ac62b91be5eb68c8c39bcab9
alluring-blog-5170 <knikadass@gmail.com>
2020-07-11 01:24:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  famous_dogs = ["Simba", "Kobe", "Bentley", "Nala"]
+  famous_dogs = ["Simba", "Kobe", "Bentley", "Nala", "arrays!"]
   famous_dogs.pop
 end
 
