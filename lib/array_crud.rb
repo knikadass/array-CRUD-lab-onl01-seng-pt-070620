871bf54852a95f8ebbc424e8e6b9743d22f76524
alluring-blog-5170 <knikadass@gmail.com>
2020-07-11 01:24:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  famous_dogs = ["wow", "Simba", "Kobe", "Bentley", "Nala"]
 end
 
 def retrieve_element_from_index(array, index_number)
