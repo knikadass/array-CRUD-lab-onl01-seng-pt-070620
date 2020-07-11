207de67f4ba7dba46dabed16435904cc6e564725
alluring-blog-5170 <knikadass@gmail.com>
2020-07-11 01:15:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  famous_dogs.push("Kimmy")
+  famous_dogs.push("Kobe")
 end
 
 def add_element_to_start_of_array(array, element)
