6b83776478e0191e19e31893d574ddbe1796c65e
alluring-blog-5170 <knikadass@gmail.com>
2020-07-11 01:30:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  famous_dogs = ["am","Simba", "Kobe", "Bentley", "Nala"]
+  famous_dogs = ["wow","Simba", "Kobe", "Bentley", "Nala"]
   famous_dogs.first
 end
 
